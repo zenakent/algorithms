e265affa43f083233ff6b3438f340203a4a2bfb2
Maui Torio <zenakent@gmail.com>
2020-08-23 10:39:45 +0800

    !==

--- SinglyLinkedList2.jsx
+++ SinglyLinkedList2.jsx
@@ -93,7 +93,7 @@ class SinglyLinkedList {
   get(index) {
     if (index < 0 || index >= this.length) return null
     let current = this.head;
-    for (let i = 0; i < index; i++) {
+    for (let i = 0; i !== index; i++) {
       current = current.next
     }
     return current;
